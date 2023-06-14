d0f037dccea11cd91a854c79caff48b3f8d46524
saijami <saijami@unosimple.com>
2023-06-14 19:01:27 +0530

    Persist

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,12 +2,21 @@ import { legacy_createStore as createStore, combineReducers } from "redux";
 import TodoReducer from "./Reducers/TodoReducer";
 import FruitsReducer from "./Reducers/FruitsReducer";
 import PersistReducer from "./Reducers/PersistReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 const rootReducer = combineReducers({
   todos: TodoReducer,
   friuts: FruitsReducer,
   persistFriuts: PersistReducer,
 });
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["persistFriuts"],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const Store = createStore(persistedReducer);
+export const persistor = persistStore(Store);
