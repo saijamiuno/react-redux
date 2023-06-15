bc3cba5c8cc2c9844a4eb3dfe06dc58d4f01d0d9
saijami <saijami@unosimple.com>
2023-06-15 15:46:25 +0530

    api data render in 5

--- src/Components/Redux-Persist/Render5.js
+++ src/Components/Redux-Persist/Render5.js
@@ -3,6 +3,8 @@ import { Row, Col, Card } from "antd";
 import { useSelector } from "react-redux";
 export default function Render5() {
   const { selectedFruit } = useSelector((state) => state.persistFriuts);
+  const { data } = useSelector((state) => state.apiData);
+  console.log(data, "data");
 
   return (
     <div>
@@ -15,6 +17,7 @@ export default function Render5() {
               title="Render 5.js"
             >
               <h1>Selected fruit {selectedFruit}</h1>
+              <h5> {data[1]?.title}</h5>
             </Card>
           </Col>
         </Row>
