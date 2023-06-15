43f36fe749f34cd53762e3f63ef9bff8c92b1342
saijami <saijami@unosimple.com>
2023-06-15 12:30:32 +0530

    persist reverted

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,21 +2,12 @@ import { legacy_createStore as createStore, combineReducers } from "redux";
 import TodoReducer from "./Reducers/TodoReducer";
 import FruitsReducer from "./Reducers/FruitsReducer";
 import PersistReducer from "./Reducers/PersistReducer";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 const rootReducer = combineReducers({
   todos: TodoReducer,
   friuts: FruitsReducer,
   persistFriuts: PersistReducer,
 });
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["persistFriuts"],
-};
+const store = createStore(rootReducer);
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const Store = createStore(persistedReducer);
-export const persistor = persistStore(Store);
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Store, persistor } from "./Redux/Store";
+
+import Store from "./Redux/Store";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <PersistGate loading={null} persistor={persistor}>
       <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
